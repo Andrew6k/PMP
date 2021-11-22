0ee6b12d6edc64af26f2549e476402de6b926bd9
Andrew6k <75756033+Andrew6k@users.noreply.github.com>
2021-11-22 13:50:15 +0200

    Add files via upload

--- src/main/scala/Lab7/Ex1.scala
+++ src/main/scala/Lab7/Ex1.scala
@@ -1,15 +1,90 @@
 package Lab7
 
+import com.cra.figaro.language.{Element, Select, Flip, Apply, Chain}
+import com.cra.figaro.library.compound.{^^}
+import com.cra.figaro.algorithm.factored.VariableElimination
 import com.cra.figaro.language._
-import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.library.atomic.discrete._
+import com.cra.figaro.algorithm.sampling.Importance
+import com.cra.figaro.library.compound.If
 
-object Ex1 {
-	def main(args: Array[String]) {
-		val test = Constant("Test")
+object Departments {
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
+	class ResearchAndDevelopment {
+		val state = FromRange(0,101)
+	}
+
+	class HumanResources {
+		val state = FromRange(0,101) 
+	}
+
+	class Production(val rd: ResearchAndDevelopment, val hr: HumanResources) {
 		
-		println(algorithm.probability(test, "Test"))
+		val state = Apply(rd.state, hr.state,
+								(state1: Int, state2: Int) =>
+									if (state1 >= 50)
+									{
+										if (state2 >= 50) 80 else 60
+									}
+									else
+									{
+										20
+									}
+			)
+
+	}
+
+	class Sales(val p: Production) {
+		val state = FromRange(0,101) 
+	}
+
+	class Finance(val hr: HumanResources, val s: Sales) {
+		
+		val state = Apply(hr.state, s.state,
+								(state1: Int, state2: Int) =>
+									if (state1 >= 50)
+									{
+										if (state2 >= 50) 90 else 50
+									}
+									else
+									{
+										10
+									}
+			) 
+	}
+
+	class Firm(val rd: ResearchAndDevelopment, val hr: HumanResources, val p: Production, val s: Sales, val f: Finance) {
+	// 	val health = Apply(rd.state, hr.state, p.state, s.state, f.state,
+	// 							(state1: Int, state2: Int, state3: Int, state4: Int, state5: Int) =>
+	// 								if (state3 >= 50)
+	// 								{
+	// 									if (state5 >= 50) 99 else 80
+	// 								}
+	// 								else
+	// 								{
+	// 									45
+	// 								}
+	// 		)
+		val health = Apply(rd.state, hr.state, p.state, s.state, f.state,
+	 							(state1: Int, state2: Int, state3: Int, state4: Int, state5: Int) =>
+								(state1+state2+state3+state4+state5)/5) 				 
+	}
+
+	def main(args: Array[String]) {
+		val rd = new ResearchAndDevelopment()
+		val hr = new HumanResources()
+		val p = new Production(rd, hr)
+		val s = new Sales(p)
+		val f = new Finance(hr, s)
+		val firm = new Firm(rd, hr, p, s, f)
+
+		p.state.observe(55)
+		f.state.observe(55)
+		val alg = Importance(200, firm.health)
+        alg.start()
+        alg.stop()
+        println("Expected health:" + alg.mean(firm.health))
+		//VariableElimination.probability(firm.health,45)
+		
 	}
 }
